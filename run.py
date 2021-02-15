03dc21041859cc4664a8b9f886c3af45b0f399be
Brett Adams <brett@ba.id.au>
2021-02-16 08:17:15 +1000

    Minor updates

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import time
 from datetime import datetime
 
 REGION = 'QLD1'
+MAX_EXPORT = 5
+MIN_EXPORT = 0
 
 payload = {'timeScale': ['5MIN']}
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
@@ -13,8 +15,8 @@ expected = math.floor((time.time()+150)/300)*300
 while True:
     response = requests.post("https://aemo.com.au/aemo/apps/api/report/5MIN", headers=headers, json=payload)
     if(response.status_code == 200):
-        qld = [x for x in response.json()['5MIN'] if x['REGION'] == REGION]
-        received = int(datetime.strptime(qld[-1]['SETTLEMENTDATE'], '%Y-%m-%dT%H:%M:%S').timestamp())-300
+        data = [x for x in response.json()['5MIN'] if x['REGION'] == REGION]
+        received = int(datetime.strptime(data[-1]['SETTLEMENTDATE'], '%Y-%m-%dT%H:%M:%S').timestamp())-300
         if(received < expected):
             print(f"No settlement yet, request took {round(response.elapsed.total_seconds(),3)}, data may be cached until {response.headers['Expires']}")
             time.sleep(7.5)
@@ -24,13 +26,12 @@ while True:
             print(f"Got data {lag} seconds late, request took {round(response.elapsed.total_seconds(),3)} seconds, updated {response.headers['Last-Modified']}")
             break
 
-price = (qld[-1]['RRP']+qld[-2]['RRP']+qld[-3]['RRP']+qld[-4]['RRP']+qld[-5]['RRP']+qld[-6]['RRP'])/6
-print(qld[-1]['SETTLEMENTDATE'],qld[-2]['SETTLEMENTDATE'],qld[-3]['SETTLEMENTDATE'],qld[-4]['SETTLEMENTDATE'],qld[-5]['SETTLEMENTDATE'],qld[-6]['SETTLEMENTDATE'])
-print(price)
+price = (data[-1]['RRP']+data[-2]['RRP']+data[-3]['RRP']+data[-4]['RRP']+data[-5]['RRP']+data[-6]['RRP'])/6
+#print(data[-1]['SETTLEMENTDATE'],data[-2]['SETTLEMENTDATE'],data[-3]['SETTLEMENTDATE'],data[-4]['SETTLEMENTDATE'],data[-5]['SETTLEMENTDATE'],data[-6]['SETTLEMENTDATE'])
 
 if(price > 0):
-    print("Export up to 5kWh")
+    print(f"Export up to {MAX_EXPORT}kWh, wholesale FiT is {price/10} c/kWh")
 else:
-    print("Export up to 0kWh")
+    print(f"Export up to {MIN_EXPORT}kWh, wholesale FiT is {price/10} c/kWh")
 
 
