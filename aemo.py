ad209f2f110e8b3ea9fccc412fb5a96b67521793
Brett Adams <brett@ba.id.au>
2021-03-27 19:41:27 +1000

    Implemented @yaleman HTTPAdapter idea

--- aemo.py
+++ aemo.py
@@ -4,7 +4,7 @@ import time
 from datetime import datetime
 
 REGION = 'QLD1'
-MAX_EXPORT = 5
+MAX_EXPORT = 5000
 MIN_EXPORT = 0
 
 payload = {'timeScale': ['5MIN']}
@@ -12,27 +12,28 @@ headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 
 expected = math.floor((time.time()+150)/300)*300
 
-while True:
-    response = requests.post("https://aemo.com.au/aemo/apps/api/report/5MIN", headers=headers, json=payload)
-    if(response.status_code == 200):
-        data = [x for x in response.json()['5MIN'] if x['REGION'] == REGION]
-        received = int(datetime.strptime(data[-1]['SETTLEMENTDATE'], '%Y-%m-%dT%H:%M:%S').timestamp())-300
-        if(received < expected):
-            print(f"No settlement yet, request took {round(response.elapsed.total_seconds(),3)}, data may be cached until {response.headers['Expires']}")
-            time.sleep(7.5)
-            continue
-        else:
-            lag = round(time.time()-received,3)
-            print(f"Got data {lag} seconds late, request took {round(response.elapsed.total_seconds(),3)} seconds, updated {response.headers['Last-Modified']}")
-            break
+with requests.Session() as s:
+    while True:
+        response = s.post("https://aemo.com.au/aemo/apps/api/report/5MIN", headers=headers, json=payload)
+        if(response.status_code == 200):
+            data = [x for x in response.json()['5MIN'] if x['REGION'] == REGION]
+            received = int(datetime.strptime(data[-1]['SETTLEMENTDATE'], '%Y-%m-%dT%H:%M:%S').timestamp())-300
+            if(received < expected):
+                print(f"No settlement yet, request took {round(response.elapsed.total_seconds(),3)}, data may be cached until {response.headers['Expires']}")
+                time.sleep(7.5)
+                continue
+            else:
+                lag = round(time.time()-received,3)
+                print(f"Got data {lag} seconds late, request took {round(response.elapsed.total_seconds(),3)} seconds, updated {response.headers['Last-Modified']}")
+                break
 
 price = (data[-1]['RRP']+data[-2]['RRP']+data[-3]['RRP']+data[-4]['RRP']+data[-5]['RRP']+data[-6]['RRP'])/6
-print(data[-1]['RRP'],data[-2]['RRP'],data[-3]['RRP'],data[-4]['RRP'],data[-5]['RRP'],data[-6]['RRP'])
-print(data[-1]['SETTLEMENTDATE'],data[-2]['SETTLEMENTDATE'],data[-3]['SETTLEMENTDATE'],data[-4]['SETTLEMENTDATE'],data[-5]['SETTLEMENTDATE'],data[-6]['SETTLEMENTDATE'])
+#print(data[-1]['RRP'],data[-2]['RRP'],data[-3]['RRP'],data[-4]['RRP'],data[-5]['RRP'],data[-6]['RRP'])
+#print(data[-1]['SETTLEMENTDATE'],data[-2]['SETTLEMENTDATE'],data[-3]['SETTLEMENTDATE'],data[-4]['SETTLEMENTDATE'],data[-5]['SETTLEMENTDATE'],data[-6]['SETTLEMENTDATE'])
 
 if(price > 0):
-    print(f"Export up to {MAX_EXPORT}kWh, wholesale FiT is expected to be {round(price/10,3)} c/kWh")
+    export = MAX_EXPORT
 else:
-    print(f"Export up to {MIN_EXPORT}kWh, wholesale FiT is expected to be {round(price/10,3)} c/kWh")
-
+    export = MIN_EXPORT
+print(f"Export up to {export} Watts, wholesale FiT is expected to be {round(price/10,3)} c/kWh")
 
--- fronius.py
+++ fronius.py
@@ -1,74 +1,33 @@
 import requests
-import re
-from requests.compat import urlparse, str, basestring
-from requests.cookies import extract_cookies_to_jar
-from requests._internal_utils import to_native_string
-from requests.utils import parse_dict_header
+#import re
+#from requests.cookies import extract_cookies_to_jar
+#from requests.utils import parse_dict_header
 
-BASE = "http://fronius"
 
-USERNAME = 'service'
-PASSWORD = ''
+from config import BASE, USERNAME, PASSWORD
 EXPORT_LIMIT = 0
 
-# Patch the HTTP Digest Auth to use the modified header
-class HTTPDigestAuth(requests.auth.HTTPDigestAuth):
-    def handle_401(self, r, **kwargs):
-        """
-        Takes the given response and tries digest-auth, if needed.
-
-        :rtype: requests.Response
-        """
-
-        # If response is not 4xx, do not auth
-        # See https://github.com/psf/requests/issues/3772
-        if not 400 <= r.status_code < 500:
-            self._thread_local.num_401_calls = 1
-            return r
-
-        if self._thread_local.pos is not None:
-            # Rewind the file position indicator of the body to where
-            # it was to resend the request.
-            r.request.body.seek(self._thread_local.pos)
-        s_auth = r.headers.get('X-WWW-Authenticate', '')
-
-        if 'digest' in s_auth.lower() and self._thread_local.num_401_calls < 2:
-
-            self._thread_local.num_401_calls += 1
-            pat = re.compile(r'digest ', flags=re.IGNORECASE)
-            self._thread_local.chal = parse_dict_header(pat.sub('', s_auth, count=1))
-
-            # Consume content and release the original connection
-            # to allow our new request to reuse the same one.
-            r.content
-            r.close()
-            prep = r.request.copy()
-            extract_cookies_to_jar(prep._cookies, r.request, r.raw)
-            prep.prepare_cookies(prep._cookies)
-
-            prep.headers['Authorization'] = self.build_digest_header(
-                prep.method, prep.url)
-            _r = r.connection.send(prep, **kwargs)
-            _r.history.append(r)
-            _r.request = prep
-
-            return _r
-
-        self._thread_local.num_401_calls = 1
-        return r
+class FroniusAdapter(requests.adapters.HTTPAdapter):
+    def build_response(self, req, resp):
+        response = super().build_response(req, resp)
+        if 'X-WWW-Authenticate' in response.headers:
+            response.headers['WWW-Authenticate'] = response.headers['X-WWW-Authenticate']
+        return response
 
 with requests.session() as s:
-    s.auth = HTTPDigestAuth(USERNAME,PASSWORD)
+    s.mount(BASE, FroniusAdapter())
+    s.auth = requests.auth.HTTPDigestAuth(USERNAME,PASSWORD)
     dno = s.get(BASE+'/config/exportlimit/')
     if not dno.ok:
+        print(dno.status_code)
         exit
-    print(dno.json()['Body']['Data'])
+
     payload = dno.json()['Body']['Data']
-    payload['exportlimit']['DPL_WLIM_ABS'] = 0
+    payload['exportlimit']['DPL_WLIM_ABS'] = EXPORT_LIMIT
 
     change = s.post(BASE+'/config/exportlimit/?method=save',json=payload)
     if change.ok and change.json()['Head']['Status']['Code'] == 0:
-        print("Done")
+        print(f"Set export limit to {EXPORT_LIMIT} Watts")
     else:
         print(change.status_code)
         print(change.json()['Head']['Status'])
